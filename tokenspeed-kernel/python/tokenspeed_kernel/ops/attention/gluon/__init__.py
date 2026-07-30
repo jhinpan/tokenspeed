2f8b89408e1603a72ee71d534aa4901493190e4e
Yu-Zhewen <zhewenyu@amd.com>
2026-07-30 15:10:07 +0000

    feat(amd): add gfx1250 Gluon MLA decode
    Signed-off-by: Yu-Zhewen <zhewenyu@amd.com>

--- tokenspeed-kernel/python/tokenspeed_kernel/ops/attention/gluon/__init__.py
+++ tokenspeed-kernel/python/tokenspeed_kernel/ops/attention/gluon/__init__.py
@@ -75,6 +75,9 @@ if current_platform().is_amd:
     from tokenspeed_kernel_amd.ops.attention.gluon.mla_decode_gfx950 import (
         gluon_mla_decode_fp8xfp8_gfx950 as _mla_decode_fp8xfp8_impl,
     )
+    from tokenspeed_kernel_amd.ops.attention.gluon.mla_decode_gfx1250 import (
+        gluon_mla_decode_bf16_gfx1250 as _mla_decode_bf16_gfx1250_impl,
+    )
     from tokenspeed_kernel_amd.ops.attention.gluon.mla_prefill_bf16_gfx950 import (
         gluon_mla_prefill_bf16_gfx950 as _mla_prefill_impl,
     )
@@ -373,6 +376,35 @@ if current_platform().is_amd:
     )
     def gluon_mla_decode_bf16xbf16_gfx950_bh64(*args, **kwargs):
         return _mla_decode_bf16xbf16_impl(*args, **kwargs)
+
+    @register_kernel(
+        "attention",
+        "mla_decode_with_kvcache",
+        name="gluon_mla_decode_bf16_gfx1250",
+        solution="gluon",
+        capability=CapabilityRequirement(
+            min_arch_version=ArchVersion(12, 5),
+            max_arch_version=ArchVersion(12, 5),
+            vendors=frozenset({"amd"}),
+        ),
+        signatures=format_signatures(
+            ("q", "kv_cache"),
+            "dense",
+            {torch.bfloat16},
+        ),
+        priority=Priority.SPECIALIZED,
+        traits={
+            "q_len": frozenset({1}),
+            "num_q_heads": frozenset(range(1, 129)),
+            "page_size": frozenset({64}),
+            "kv_lora_rank": frozenset({512}),
+            "qk_rope_head_dim": frozenset({64}),
+            "support_logit_cap": frozenset({False}),
+            "return_lse": frozenset({False, True}),
+        },
+    )
+    def gluon_mla_decode_bf16_gfx1250(*args, **kwargs):
+        return _mla_decode_bf16_gfx1250_impl(*args, **kwargs)
 
     @register_kernel(
         "attention",
